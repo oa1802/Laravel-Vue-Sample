f7f3385af50089d240813895b1b5a955251e5067
oa1802 <omaralkha@gmail.com>
2020-05-12 12:36:21 -0400

    Updated layout.blade.php secure CSS link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
    <head>
         <title>Omar's Classic Books - @yield('title')</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" />
 	    <script defer src="{{ mix('js/app.js') }}"></script>
    </head>
    <body>
